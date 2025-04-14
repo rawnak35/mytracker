231075708b2d13612e7e574456af24a41e36d39d
Rawnak Sabek <rawnaks@xsense.co>
2025-04-14 14:04:45 +0300

    controller: Add transaction controller

---
+++ src/main/java/com/finTrack/mytracker/controller/TransactionController.java
@@ -0,0 +1,25 @@
+package com.finTrack.mytracker.controller;
+
+import com.finTrack.mytracker.dto.TransactionDto;
+import com.finTrack.mytracker.service.TransactionsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/transactions")
+@AllArgsConstructor
+public class TransactionController {
+
+    private TransactionsService transactionsService;
+    @PostMapping("/create")
+    public ResponseEntity<TransactionDto> create(@RequestBody TransactionDto transactionDto) {
+        TransactionDto savedTrans = transactionsService.createTrans(transactionDto);
+        return new ResponseEntity<>(savedTrans, HttpStatus.CREATED);
+    }
+
+}
